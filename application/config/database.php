58b55d6619cdae714b4466871ae91150de7df01c
Dedi Hartono <dedihartono1993@gmail.com>
2017-10-12 01:39:12 +0700

    setting heroku

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '123456',
-	'database' => 'db_siobat',
+	'hostname' => 'sql12.freemysqlhosting.net',
+	'username' => 'sql12198920',
+	'password' => 'MycZ39LHSc',
+	'database' => 'sql12198920',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
